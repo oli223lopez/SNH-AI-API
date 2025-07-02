204ccde68da74b9905ef20b55c202c13fd56f9f1
Oliver <reviloz223@gmail.com>
2025-07-02 13:42:02 -0400

    initialize db at server start

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 const app = express();
 const port = 2323;
-import router from "./routers/treeRouter.ts";
+import {treeRouter} from "./routes/treeRouter.ts";
+import { TreeController } from "./controllers/treeController.ts";
+import { dbPromise } from "./db/index.ts";
+
+const treeController = new TreeController( await dbPromise);
+
 
 app.use(express.json());
-app.use(router);
+app.use(treeRouter(treeController));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
