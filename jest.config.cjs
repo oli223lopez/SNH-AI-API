d4fc933b6c33933c97521fde7461c69329142863
Oliver <reviloz223@gmail.com>
2025-07-02 00:37:05 -0400

    set up tests

---
+++ jest.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest/presets/js-with-ts-esm',
+  testEnvironment: 'node',
+  rootDir: '.', // Set to project root (directory containing package.json)
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.ts$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+};
